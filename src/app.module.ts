0294597c86d3279a0ae0fa1a3e5a61c2cb0d38a0
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-12 14:55:48 +0100

    feat: update app.module with all other modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { HealthModule } from './health/health.module';
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TerminusModule,
+    ScheduleModule.forRoot(),
+    DbModule,
+    UtilsModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
