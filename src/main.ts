09b7b7ff8a795530e04bf433aea70868cc3c8959
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-12 15:03:24 +0100

    feat: update main.ts file with microservice setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+import { microserviceOptions } from './utils/microservice-connection';
+
+const logger = new Logger('Microservice');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
+
+  await app.listen(() => {
+    logger.log('Microservice is listening');
+  });
 }
 bootstrap();
