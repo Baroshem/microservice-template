5d9c2a6a638a463d61e7aa6f667a10034c1239a2
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-12 16:57:35 +0100

    feat: include all modules and services in item.module

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ItemRepository } from '../db/repositories';
+import { CommandHandlers } from './commands/handlers';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
+import { QueryHandlers } from './queries/handlers';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ItemRepository]), CqrsModule],
   controllers: [ItemController],
-  providers: [ItemService]
+  providers: [
+    ItemService,
+    ...QueryHandlers,
+    ...CommandHandlers,
+    ConfigService
+  ]
 })
 export class ItemModule {}
