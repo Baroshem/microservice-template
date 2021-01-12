3caf66517c9e2ccc292ca0a61472001e26e339e5
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-12 14:33:17 +0100

    feat: add basic ItemEntity

---
+++ src/db/entities/item.entity.ts
@@ -0,0 +1,15 @@
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class ItemEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
