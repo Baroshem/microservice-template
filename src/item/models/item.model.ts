43c50a0dad5c1615e47d86bfeb6fe24ef5a8c224
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-25 15:20:38 +0100

    feat-#2: add item model

---
+++ src/item/models/item.model.ts
@@ -0,0 +1,13 @@
+import { AggregateRoot } from "@nestjs/cqrs";
+
+import { ItemDeletedEvent } from "../events/impl";
+
+export class Item extends AggregateRoot {
+    constructor(private readonly id: number) {
+        super();
+    }
+
+    deleteItem(itemId: number) {
+        this.apply(new ItemDeletedEvent(itemId));
+    }
+}
