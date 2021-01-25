2a7340006226f9b8813fe491a863c66b00aae0e7
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-25 15:44:47 +0100

    feat-#2: created event handler for delete item

---
+++ src/item/events/handlers/item-deleted.handler.ts
@@ -0,0 +1,10 @@
+import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
+
+import { ItemDeletedEvent } from "../impl";
+
+@EventsHandler(ItemDeletedEvent)
+export class ItemDeletedHandler implements IEventHandler<ItemDeletedEvent> {
+    handle(event: ItemDeletedEvent) {
+        console.log(`Handled ItemDeletedEvent with ID: ${event.itemId}`);
+    }
+}
