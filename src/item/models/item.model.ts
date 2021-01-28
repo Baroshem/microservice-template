b33c3a99d44ed194ff8401de261a6cb10b6be3bc
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-28 18:20:56 +0100

    feat-#5: update item model

--- src/item/models/item.model.ts
+++ src/item/models/item.model.ts
@@ -1,13 +1,18 @@
 import { AggregateRoot } from '@nestjs/cqrs';
+import { CreateItemDto } from '../dtos';
 
-import { ItemDeletedEvent } from '../events/impl';
+import { ItemCreatedEvent, ItemDeletedEvent } from '../events/impl';
 
 export class Item extends AggregateRoot {
-  constructor(private readonly id: number) {
+  constructor(private readonly id?: number) {
     super();
   }
 
   deleteItem() {
     this.apply(new ItemDeletedEvent(this.id));
+  }
+
+  createItem(createItemDto: CreateItemDto) {
+    this.apply(new ItemCreatedEvent(createItemDto));
   }
 }
