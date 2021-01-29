d021ce5d6ccb0ec749c024697fcd87c7efa80cca
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-29 09:16:49 +0100

    feat-#9: adjust mockedConfigService

--- src/utils/mocks/mocked-config-service.ts
+++ src/utils/mocks/mocked-config-service.ts
@@ -1,14 +1,26 @@
 export const mockedConfigService = {
-  get(key: string): string {
+  get(key: string): string | number {
     switch (key) {
-      case 'JWT_ACCESS_TOKEN_SECRET':
-        return 'top-secret-access';
-      case 'JWT_ACCESS_TOKEN_EXPIRATION_TIME':
-        return '240s';
-      case 'JWT_REFRESH_TOKEN_SECRET':
-        return 'top-secret-refresh';
-      case 'JWT_REFRESH_TOKEN_EXPIRATION_TIME':
-        return '200s';
+      case 'POSTGRES_USERNAME':
+        return 'postgres_user';
+      case 'POSTGRES_PASSWORD':
+        return 'postgres_password';
+      case 'POSTGRES_DATABASE':
+        return 'item';
+      case 'POSTGRES_PORT':
+        return 5433;
+      case 'POSTGRES_HOST':
+        return 'localhost';
+      case 'RABBITMQ_DEFAULT_USER':
+        return 'rabbitmq';
+      case 'RABBITMQ_DEFAULT_PASS':
+        return 'rabbitmq';
+      case 'RABBITMQ_DEFAULT_VHOST':
+        return 'otasoft-api';
+      case 'RABBITMQ_NODENAME':
+        return 'localhost';
+      case 'RABBITMQ_FIRST_HOST_PORT':
+        return 5673
     }
   },
 };
