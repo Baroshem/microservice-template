bfb67ec134453e86a805becf5a74dc9e83a61438
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-30 17:22:35 +0100

    feat-#10: update saga with new command

--- src/item/sagas/item.sagas.ts
+++ src/item/sagas/item.sagas.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ofType, Saga } from '@nestjs/cqrs';
+import { ICommand, ofType, Saga } from '@nestjs/cqrs';
 import { Observable } from 'rxjs';
 import { delay, map } from 'rxjs/operators';
 
+import { NotifyItemOwnerCommand } from '../commands/impl';
 import {
   ItemCreatedEvent,
   ItemUpdatedEvent,
@@ -12,35 +13,41 @@ import {
 @Injectable()
 export class ItemSagas {
   @Saga()
-  itemCreated = (events$: Observable<any>) => {
-    return (
-      events$.pipe(ofType(ItemCreatedEvent)),
-      delay(1000),
-      map((event) => {
-        console.log(`Inside itemCreated @Saga: ${event}`);
-      })
-    );
-  };
+  itemCreated = (events$: Observable<any>): Observable<ICommand> => {
+    return events$
+      .pipe(
+        ofType(ItemCreatedEvent),
+        delay(1000),
+        map(event => {
+          console.log(`Inside itemCreated @Saga: ${event}`)
+          return new NotifyItemOwnerCommand(event);
+        }),
+      );
+  }
 
   @Saga()
-  itemUpdated = (events$: Observable<any>) => {
-    return (
-      events$.pipe(ofType(ItemUpdatedEvent)),
-      delay(1000),
-      map((event) => {
-        console.log(`Inside itemUpdated @Saga: ${event}`);
-      })
-    );
-  };
+  itemUpdated = (events$: Observable<any>): Observable<ICommand> => {
+    return events$
+      .pipe(
+        ofType(ItemUpdatedEvent),
+        delay(1000),
+        map(event => {
+          console.log(`Inside itemUpdated @Saga: ${event}`);
+          return new NotifyItemOwnerCommand(event);
+        }),
+      );
+  }
 
   @Saga()
-  itemDeleted = (events$: Observable<any>) => {
-    return (
-      events$.pipe(ofType(ItemDeletedEvent)),
-      delay(1000),
-      map((event) => {
-        console.log(`Inside itemDeleted @Saga: ${event}`);
-      })
-    );
-  };
+  itemDeleted = (events$: Observable<any>): Observable<ICommand> => {
+    return events$
+      .pipe(
+        ofType(ItemDeletedEvent),
+        delay(1000),
+        map(event => {
+          console.log(`Inside itemDeleted @Saga: ${event}`);
+          return new NotifyItemOwnerCommand(event);
+        }),
+      );
+  }
 }
