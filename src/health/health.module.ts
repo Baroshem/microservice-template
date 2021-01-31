a5a9d45dc5b4e139ab2dd4a97014ba9bf3afe3b8
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-31 22:11:28 +0100

    feat-#12: move health services to services folder

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TerminusModule } from '@nestjs/terminus';
 
-import { HealthService } from './health.service';
+import { HealthService } from './services';
 
 @Module({
   imports: [TerminusModule, ScheduleModule.forRoot()],
