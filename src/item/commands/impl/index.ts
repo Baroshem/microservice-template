c410805ca4412c482d9e4dcc57b3f6d4a7ccc93b
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-31 15:27:20 +0100

    feat-#10: fix typo in command name

--- src/item/commands/impl/index.ts
+++ src/item/commands/impl/index.ts
@@ -1,4 +1,4 @@
 export * from './delete-item-by-id.command';
 export * from './create-item.command';
 export * from './update-item.command';
-export * from './notyify-item-owner.command';
+export * from './notify-item-owner.command';
