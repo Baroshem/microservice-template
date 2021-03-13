761a5c64c6b8d20a059c905038f9b61bcbdfffe5
Adam Oleszko <adamoleszko44@gmail.com>
2021-03-13 23:57:27 +0100

    feat-#35: Fix typo in ItemEntity

--- src/item/infrastructure/entities/item.entity.spec.ts
+++ src/item/infrastructure/entities/item.entity.spec.ts
@@ -1,10 +1,10 @@
 import { ItemEntity } from './item.entity';
 
 describe('ItemEntity', () => {
-  it('should create ItemEnitity object', () => {
-    const itemEnitity = new ItemEntity(1, 'Test');
-    expect(itemEnitity.id).toBe(1);
-    expect(itemEnitity.name).toBe('Test');
-    expect(itemEnitity instanceof ItemEntity).toBe(true);
+  it('should create ItemEntity object', () => {
+    const itemEntity = new ItemEntity(1, 'Test');
+    expect(itemEntity.id).toBe(1);
+    expect(itemEntity.name).toBe('Test');
+    expect(itemEntity instanceof ItemEntity).toBe(true);
   });
 });
